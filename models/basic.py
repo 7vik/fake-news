4bb30c71b658c6c8568a96c326ba3a990c768bb9
Satvik Golechha <f20170117@pilani.bits-pilani.ac.in>
2019-01-13 21:42:36 +0530

    Correct formatting of dataset

--- models/basic.py
+++ models/basic.py
@@ -5,12 +5,14 @@ import pandas as pd
 import os
 import general_funcs
 
-path_to_data = os.path.abspath("./../datasets/wndr_dataset.csv")
+path_to_data = os.path.abspath("./../datasets/final_dataset.csv")
 data = pd.read_csv(path_to_data)
-data = data.dropna(axis=0)
-data["tok_cont"] = data.apply(lambda row: nltk.word_tokenize(row["Content"]),axis=1)
-data["tok_title"] = data.apply(lambda row: nltk.word_tokenize(row["Title"]),axis=1)
-data["pos_cont"] = data.apply(lambda row: nltk.pos_tag(row["tok_cont"]),axis=1)
-data["pos_title"] = data.apply(lambda row: nltk.pos_tag(row["tok_title"]),axis=1)
-data["wc_cont"] = data.apply(lambda row: set(row["tok_cont"]),axis=1)
-data["wc_title"] = data.apply(lambda row: set(row["tok_title"]),axis=1)
+#data = data.dropna(axis=0)
+#data["tok_cont"] = data.apply(lambda row: nltk.word_tokenize(row["Content"]),axis=1)
+#data["tok_title"] = data.apply(lambda row: nltk.word_tokenize(row["Title"]),axis=1)
+#data["pos_cont"] = data.apply(lambda row: nltk.pos_tag(row["tok_cont"]),axis=1)
+#data["pos_title"] = data.apply(lambda row: nltk.pos_tag(row["tok_title"]),axis=1)
+#data["wc_cont"] = data.apply(lambda row: set(row["tok_cont"]),axis=1)
+#data["wc_title"] = data.apply(lambda row: set(row["tok_title"]),axis=1)
+
+print(data.head())
