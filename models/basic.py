9dea023821910ed8620765802786deb7760a2ffb
Siddharth Singh <sidharth_0903@hotmail.com>
2019-01-13 22:24:34 +0530

    BUG FIX: Fixed value error in data

--- models/basic.py
+++ models/basic.py
@@ -1,16 +1,17 @@
- 
+
 import nltk
 import pandas as pd
 import os
 import general_funcs
 
 path_to_data = os.path.abspath("./../datasets/final_dataset.csv")
-data = pd.read_csv(path_to_data)
-data = data.dropna(axis=0)
-data["tok_cont"] = data.apply(lambda row: nltk.word_tokenize(row["Content"]),axis=1)
+data = pd.read_csv(path_to_data, encoding="ISO-8859-1")
+data = data.dropna(axis="columns",how="all")
+data = data.dropna(axis="rows", subset=["Title","Text"])
+data = data.dropna(axis="columns")
+data["tok_cont"] = data.apply(lambda row: nltk.word_tokenize(row["Text"]),axis=1)
 data["tok_title"] = data.apply(lambda row: nltk.word_tokenize(row["Title"]),axis=1)
 data["pos_cont"] = data.apply(lambda row: nltk.pos_tag(row["tok_cont"]),axis=1)
 data["pos_title"] = data.apply(lambda row: nltk.pos_tag(row["tok_title"]),axis=1)
 data["wc_cont"] = data.apply(lambda row: set(row["tok_cont"]),axis=1)
 data["wc_title"] = data.apply(lambda row: set(row["tok_title"]),axis=1)
-
