de0044b6c4f107b0b2b6c239e8aabb8e7444c869
Siddharth Singh <sidharth_0903@hotmail.com>
2019-01-17 11:13:45 +0530

    Generated IBM data converted to csv

--- ibm-api-connect/make_natlang_csv.py
+++ ibm-api-connect/make_natlang_csv.py
@@ -5,16 +5,16 @@ import csv
 
 
 
-name_ = 'keyw_fakeNews_fake.csv'
+name_ = 'keyw_celeb_fake.csv'
 f = open(name_, 'w+')
 outWrite = csv.writer(f)
 
 
-path_ = './natlang_responses/fakeNewsDataset/fake'
+path_ = './natlang_responses/celebrityDataset/fake'
 for file_ in os.listdir(path_):
     with open(os.path.join(path_, file_), 'r+') as f:
         res = json.load(f)
-    row10 = [res['categories'][0]['label'], res['categories'][0]['score']]
+    row10 = [file_, res['categories'][0]['label'], res['categories'][0]['score']]
     for concept in res['concepts']:
         row10.append(concept['text'])
         row10.append(concept['relevance'])
